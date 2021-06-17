4d8a7baf6a90fee8f270566466425f5fb7105858
Louis <louis.rivera.se@gmail.com>
2021-06-16 20:01:16 -0500

    Establish the REST endpoints
    The application will need to listen for incoming GET, PUT, POST, and DELETE requests.
    The application now responds with a temporary message indicating method used
    by the request.

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ const port = 8080;
 var displayStartServerMessage = true;
 
 const server = http.createServer(function(request, response) {
-    
+    var method;
+    if(request.method === 'GET') {
+        method = "GET detected";
+    }
+    else if(request.method === 'PUT') {
+        method = "PUT detected"
+    }
+    else if(request.method === 'POST') {
+        method = "POST detected"
+    }
+    else if(request.method === 'DELETE') {
+        method = "DELETE detected"
+    }
+    else {
+        method = "The request method wasn't defined"
+    }
+
+    response.end(method);
 }).listen(port, hostname);
 
+
+server.on('error', function(e) {
+    sendServerStartMessage = false;
+    console.log("The http server could not be created\n");
+    console.log(e);
+    server.close();
+});
 
 if(displayStartServerMessage) {
     console.log(`Server running at http://${hostname}:${port}/\n`);
